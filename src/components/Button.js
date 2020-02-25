54883d70cf8971c04315620c5fb4493c4f13927a
Luis Preza <preza.luis@gmail.com>
2020-02-24 20:14:16 -0600

    interaction click handlers added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Button = ({ name, color, wide }) => (
-  <div style={{ backgroundColor: color, width: wide ? '50%' : '25%' }}>
+const Button = ({
+  name, color, wide, onClick,
+}) => (
+  <div
+    style={{ backgroundColor: color, width: wide ? 'calc(50% + 2px)' : '25%', cursor: 'pointer' }}
+    onClick={() => onClick(name)}
+    onKeyDown={() => onClick(name)}
+    role="presentation"
+  >
     <span>{name}</span>
 
   </div>
@@ -11,12 +18,14 @@ Button.propTypes = {
   name: PropTypes.string,
   color: PropTypes.string,
   wide: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   wide: false,
   name: '',
   color: '#F5913E',
+  onClick: () => {},
 };
 
 export default Button;
